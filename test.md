9aeae3310649e4edce92bf382fb0567460334c9e
Akuli <akuviljanen17@gmail.com>
2017-09-14 18:48:38 +0300

    test

--- test.md
+++ test.md
@@ -1,5 +1,5 @@
-lel ![f'(x)=\frac{dy}{dx}=\frac{f(x+dx)-f(x)}{dx}=\lim_{h\rightarrow\infty}\frac{f(x+h)-f(x)}{h}][] like
+btw did u know that ![math:f'(x)=\frac{dy}{dx}][] where ![math:y=f(x)][] like yeah
 
-![wut wut](wat.gif)
-
-[f'(x)=\frac{dy}{dx}=\frac{f(x+dx)-f(x)}{dx}=\lim_{h\rightarrow\infty}\frac{f(x+h)-f(x)}{h}]: wat.gif
+[math:f'(x)=\frac{dy}{dx}]: images/math/a41e488bc9c34839ad0ba2c2154ae521.gif
+[math:y=f(x)]: images/math/7c1c9491ba7c6e8d6d2cfa82e39b22ca.gif
+[math:y=f(x)]: images/math/7c1c9491ba7c6e8d6d2cfa82e39b22ca.gif
---
+++ update-math-images.py
@@ -0,0 +1,81 @@
+# update the gifs in images/math and the markdown files that use them
+#
+# this downloads images from latex2png.com and it doesn't work 100% of
+# the time, run the script again if the downloading fails
+import glob
+import hashlib
+import os
+import re
+import shutil
+import urllib.request     # because i can
+
+
+MATH_REGEX = re.compile(r'!\[math:(.+?)\]\[\]')
+LINK_LIST_REGEX = re.compile(
+    r'^\[math:.+\]: images/math/[0-9a-f]{32}.gif$', re.MULTILINE)
+
+
+# the images need to be downloaded because the latex2png server seems to
+# be so slow that images linked from it don't work on github
+def download_image(math):
+    md5 = hashlib.md5(math.encode('utf-8')).hexdigest()
+    path = os.path.join('images', 'math', md5 + '.gif')
+    if os.path.exists(path):
+        return path
+
+    url = 'https://latex.codecogs.com/gif.latex?' + math.replace(' ', '&space;')
+    print("Downloading", url, "to", path, "...")
+
+    response = urllib.request.urlopen(url)
+    with open(path, 'wb') as file:
+        shutil.copyfileobj(response, file)
+
+    print("  done.")
+    return path
+
+
+def update_link_list(old_content, used_filenames):
+    maths = MATH_REGEX.findall(old_content)
+    if not maths:
+        return LINK_LIST_REGEX.sub('', old_content)
+
+    lines = []
+    for math in maths:
+        filename = download_image(math)
+        used_filenames.add(filename)
+        lines.append('[math:%s]: %s' % (math, filename.replace(os.sep, '/')))
+
+    match = LINK_LIST_REGEX.search(old_content)
+    if not match:
+        return old_content + '\n' + '\n'.join(lines) + '\n'
+
+    start, end = match.span()
+    return old_content[:start] + '\n'.join(lines) + old_content[end:]
+
+
+def main():
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    image_paths = set()
+    for filename in glob.glob('*.md'):
+        with open(filename, 'r') as file:
+            old_content = file.read()
+
+        new_content = update_link_list(old_content, image_paths)
+        if new_content == old_content:
+            print("Already OK:", filename)
+        else:
+            print("Updating", filename, "...")
+            with open(filename, 'w') as file:
+                file.write(new_content)
+
+    # delete md5 named images that aren't needed anymore
+    for filename in os.listdir(os.path.join('images', 'math')):
+        path = os.path.join('images', 'math', filename)
+        if path not in image_paths:
+            print("Deleting", path)
+            os.remove(path)
+
+
+if __name__ == '__main__':
+    main()
