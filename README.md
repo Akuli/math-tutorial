dc13b6ca4cf010c2a418d008978cda86bf75dc3c
Akuli <akuviljanen17@gmail.com>
2017-09-17 22:52:01 +0300

    publishing script

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
 # Math for Programmers
 
-This tutorial will soon be hosted on
+You can read this tutorial on
 [akuli.github.io/math-tutorial](https://akuli.github.io/math-tutorial).
 
-## Building
+## Updating the github.io site
 
-I'll create a publishing script soon, but while you're waiting for it
-you can clone the repository and build the docs locally. You need Python
-3, Sphinx and some LaTeX stuff that I'm not sure what exactly it was
-that made things work.
+You need to have Python 3, Sphinx and some LaTeX stuff installed. I
+installed a bunch of LaTeX stuff when I tried to get this to work and
+I'm not sure what exactly you need.
 
-    $ git clone https://github.com/Akuli/math-tutorial
-    $ cd math-tutorial
-    $ python3 -m sphinx . _build
-    $ ( yourwebbrowser _build/index.html &>/dev/null )&
+1. Run sphinx.
+
+        $ cd math-tutorial
+        $ python3 -m sphinx . _build
+
+2. View the output in a web browser and make sure that everything's OK.
+
+        $ ( yourfavoritebrowser _build/index.html &>/dev/null )&
+
+3. Publish it.
+
+        $ python3 publish.py
---
+++ publish.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+"""Commit the _build directory to the gh-pages branch.
+
+This script is crazy. Use it if you dare!
+"""
+
+import contextlib
+import functools
+import os
+import re
+import shlex
+import shutil
+import subprocess
+import sys
+import tempfile
+
+info = functools.partial(print, '**** %s:' % sys.argv[0])
+
+
+def run(*command, capture=False):
+    info("running", ' '.join(map(shlex.quote, command)))
+    if capture:
+        output = subprocess.check_output(list(command))
+        return output.decode('utf-8', errors='replace')
+
+    subprocess.check_call(list(command))
+    return None     # pep-8
+
+
+@contextlib.contextmanager
+def switch_branch(new_branch):
+    output = run('git', 'branch', capture=True)
+    [old_branch] = re.findall('^\* (.*)$', output, flags=re.MULTILINE)
+    run('git', 'checkout', new_branch)
+
+    try:
+        yield
+    except Exception as e:
+        # undo everything to make sure that going back to old_branch works
+        run('git', 'reset', 'HEAD', '.')
+        run('git', 'checkout', '--', '.')
+        raise e
+    finally:
+        run('git', 'checkout', old_branch)
+
+
+@contextlib.contextmanager
+def git_stash():
+    # git stash doesn't work if there's nothing to stash
+    open('stash-dummy', 'x').close()
+
+    run('git', 'stash', '--include-untracked')
+    try:
+        yield
+    finally:
+        run('git', 'stash', 'pop')
+        os.remove('stash-dummy')
+
+
+def main():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        info("copying _build to a temporary directory")
+        tmpdir = os.path.join(tmpdir, '_build')
+        shutil.copytree('_build', tmpdir)
+
+        with git_stash():
+            with switch_branch('gh-pages'):
+                for item in os.listdir():
+                    if item[0] != '.':
+                        info('removing', item)
+                        if os.path.isdir(item):
+                            shutil.rmtree(item)
+                        else:
+                            os.remove(item)
+
+                for item in os.listdir(tmpdir):
+                    if item[0] != '.' or item == '.nojekyll':
+                        info('copying', item)
+                        src = os.path.join(tmpdir, item)
+                        if os.path.isdir(src):
+                            shutil.copytree(src, item)
+                        else:
+                            shutil.copy(src, item)
+
+                run('git', 'add', '--all', '.')
+                run('git', 'commit', '-m', 'updating docs with ' + __file__)
+
+        info("deleting the temporary directory")
+
+    #run('git', 'push', 'origin', 'gh-pages')
+
+
+if __name__ == '__main__':
+    main()
