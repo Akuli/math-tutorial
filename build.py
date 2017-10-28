a617d3c40d29c94f8648ad6efcb6f1f54bedb69a
Akuli <akuviljanen17@gmail.com>
2017-10-28 17:30:40 +0300

    move stuff to a new more integrals chapter

--- build.py
+++ build.py
@@ -57,8 +57,8 @@ def get_sidebar_content(txtfile):
 
     thingy = functools.partial(_create_sidebar_thingy, txtfile)
     return ''.join([
-        thingy("Chapters", ['derivatives', 'integrals', 'geometry-and-trig',
-                            'numbertheory']),
+        thingy("Chapters", ['derivatives', 'integrals', 'more-integrals',
+                            'geometry-and-trig', 'numbertheory']),
         thingy("Other stuff", ['basics', 'summary', 'graphs', 'explanations'],
                indexlink),
     ])
